c926b8b25c1ac63d26244ec0a1e5ed87be06d6af
Tanish Tuteja <48507391+TanishTuteja@users.noreply.github.com>
2020-04-24 00:08:47 +0530

    Update DimensionError to handle static

--- src/Errors.js
+++ src/Errors.js
@@ -1,17 +1,16 @@
 class DimensionError extends Error {
-  constructor(functionName, m1, m2) {
-    super(
-      "Different Dimensions in " +
-        functionName +
-        ". Dimensions of object => " +
-        m1.rows +
-        "x" +
-        m1.cols +
-        " while dimensions of passed argument => " +
-        m2.rows +
-        "x" +
-        m2.cols
-    );
+  constructor(isStatic, functionName, m1, m2) {
+    let message = "Different Dimensions in " + functionName + ". ";
+    let dim1 = m1.rows + "x" + m1.cols;
+    let dim2 = m2.rows + "x" + m2.cols;
+    
+    if (isStatic){
+      message += "Dimensions of matrix 1 => " + dim1 + " while dimensions of matrix 2 => " + dim2;
+    } else {
+      message += "Dimensions of object => " + dim1 + " while dimensions of passed argument => " + dim2;
+    }
+    
+    super(message);
   }
 }
 

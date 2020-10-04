eec25e76738f2b108b26497673431d1813e7a1cc
TanishTuteja <tanishtuteja@gmail.com>
2020-10-04 12:26:15 +0530

    Added determinant calc + Organized Tests

--- tests/matrix.test.js
+++ tests/matrix.test.js
@@ -230,366 +230,469 @@ describe("toArray Tests", () => {
   });
 });
 
-test("non-static addElementwise test", () => {
-  let m1 = new Matrix(3, 2);
-  m1.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
+describe("fromArray Tests", () => {
+  test("static fromArray test normal", () => {
+    let array = [12, 10, 5, 4, 3];
+    let m = Matrix.fromArray(array);
 
-  let m2 = new Matrix(3, 2);
-  m2.matrix = [
-    [12, 0.9],
-    [500, 43],
-    [-1, -0.5],
-  ];
+    expect(m).toEqual({
+      rows: 5,
+      cols: 1,
+      matrix: [[12], [10], [5], [4], [3]],
+    });
+  });
 
-  m1.addElementwise(m2);
-
-  expect(m1).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [22, -14.1],
-      [512, 43.5],
-      [4, 15.5],
-    ],
+  test("static fromArray test argument non-array", () => {
+    expect.assertions(2);
+    try {
+      let m = Matrix.fromArray(2);
+    } catch (error) {
+      expect(error).toBeInstanceOf(MatrixTypeError);
+      expect(error).toHaveProperty("message", "Invalid types in fromArray, array expected");
+    }
   });
 });
 
-test("non-static addScalar test", () => {
-  let m = new Matrix(3, 2);
-  m.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
+describe("Addition Tests", () => {
+  test("non-static addElementwise test", () => {
+    let m1 = new Matrix(3, 2);
+    m1.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
 
-  let scalar = -9;
+    let m2 = new Matrix(3, 2);
+    m2.matrix = [
+      [12, 0.9],
+      [500, 43],
+      [-1, -0.5],
+    ];
 
-  m.addScalar(scalar);
+    m1.addElementwise(m2);
 
-  expect(m).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [1, -24],
-      [3, -8.5],
-      [-4, 7],
-    ],
+    expect(m1).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [22, -14.1],
+        [512, 43.5],
+        [4, 15.5],
+      ],
+    });
+  });
+
+  test("non-static addScalar test", () => {
+    let m = new Matrix(3, 2);
+    m.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
+
+    let scalar = -9;
+
+    m.addScalar(scalar);
+
+    expect(m).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [1, -24],
+        [3, -8.5],
+        [-4, 7],
+      ],
+    });
+  });
+
+  test("static addElementwise test", () => {
+    let m1 = new Matrix(3, 2);
+    m1.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
+
+    let m2 = new Matrix(3, 2);
+    m2.matrix = [
+      [12, 0.9],
+      [500, 43],
+      [-1, -0.5],
+    ];
+
+    let result = Matrix.addElementwise(m1, m2);
+
+    expect(result).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [22, -14.1],
+        [512, 43.5],
+        [4, 15.5],
+      ],
+    });
+  });
+
+  test("static addScalar test", () => {
+    let m = new Matrix(3, 2);
+    m.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
+
+    let scalar = -9;
+
+    let result = Matrix.addScalar(m, scalar);
+
+    expect(result).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [1, -24],
+        [3, -8.5],
+        [-4, 7],
+      ],
+    });
   });
 });
 
-test("non-static subtractElementwise test", () => {
-  let m1 = new Matrix(3, 2);
-  m1.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
+describe("Subtration Tests", () => {
+  test("non-static subtractElementwise test", () => {
+    let m1 = new Matrix(3, 2);
+    m1.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
 
-  let m2 = new Matrix(3, 2);
-  m2.matrix = [
-    [-12, -0.9],
-    [-500, -43],
-    [1, 0.5],
-  ];
+    let m2 = new Matrix(3, 2);
+    m2.matrix = [
+      [-12, -0.9],
+      [-500, -43],
+      [1, 0.5],
+    ];
 
-  m1.subtractElementwise(m2);
+    m1.subtractElementwise(m2);
 
-  expect(m1).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [22, -14.1],
-      [512, 43.5],
-      [4, 15.5],
-    ],
+    expect(m1).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [22, -14.1],
+        [512, 43.5],
+        [4, 15.5],
+      ],
+    });
+  });
+
+  test("non-static subtractScalar test", () => {
+    let m = new Matrix(3, 2);
+    m.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
+
+    let scalar = 9;
+
+    m.subtractScalar(scalar);
+
+    expect(m).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [1, -24],
+        [3, -8.5],
+        [-4, 7],
+      ],
+    });
+  });
+
+  test("static subtractElementwise test", () => {
+    let m1 = new Matrix(3, 2);
+    m1.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
+
+    let m2 = new Matrix(3, 2);
+    m2.matrix = [
+      [-12, -0.9],
+      [-500, -43],
+      [1, 0.5],
+    ];
+
+    let result = Matrix.subtractElementwise(m1, m2);
+
+    expect(result).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [22, -14.1],
+        [512, 43.5],
+        [4, 15.5],
+      ],
+    });
+  });
+
+  test("static subtractScalar test", () => {
+    let m = new Matrix(3, 2);
+    m.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
+
+    let scalar = 9;
+
+    let result = Matrix.subtractScalar(m, scalar);
+
+    expect(result).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [1, -24],
+        [3, -8.5],
+        [-4, 7],
+      ],
+    });
   });
 });
 
-test("non-static subtractScalar test", () => {
-  let m = new Matrix(3, 2);
-  m.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
+describe("Multiplication Tests", () => {
+  test("non-static multiplyElementwise test", () => {
+    let m1 = new Matrix(3, 2);
+    m1.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
 
-  let scalar = 9;
+    let m2 = new Matrix(3, 2);
+    m2.matrix = [
+      [-12, -0.9],
+      [-500, -43],
+      [1, 0.5],
+    ];
 
-  m.subtractScalar(scalar);
+    m1.multiplyElementwise(m2);
 
-  expect(m).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [1, -24],
-      [3, -8.5],
-      [-4, 7],
-    ],
+    expect(m1).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [-120, 13.5],
+        [-6000, -21.5],
+        [5, 8],
+      ],
+    });
+  });
+
+  test("non-static multiplyScalar test", () => {
+    let m = new Matrix(3, 2);
+    m.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
+
+    let scalar = -2;
+
+    m.multiplyScalar(scalar);
+
+    expect(m).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [-20, 30],
+        [-24, -1],
+        [-10, -32],
+      ],
+    });
+  });
+
+  test("static multiplyElementwise test", () => {
+    let m1 = new Matrix(3, 2);
+    m1.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
+
+    let m2 = new Matrix(3, 2);
+    m2.matrix = [
+      [-12, -0.9],
+      [-500, -43],
+      [1, 0.5],
+    ];
+
+    let result = Matrix.multiplyElementwise(m1, m2);
+
+    expect(result).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [-120, 13.5],
+        [-6000, -21.5],
+        [5, 8],
+      ],
+    });
+  });
+
+  test("static multiplyScalar test", () => {
+    let m = new Matrix(3, 2);
+    m.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
+
+    let scalar = -2;
+
+    let result = Matrix.multiplyScalar(m, scalar);
+
+    expect(result).toEqual({
+      rows: 3,
+      cols: 2,
+      matrix: [
+        [-20, 30],
+        [-24, -1],
+        [-10, -32],
+      ],
+    });
   });
 });
 
-test("non-static multiplyElementwise test", () => {
-  let m1 = new Matrix(3, 2);
-  m1.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
+describe("Matrix Product Tests", () => {
+  test("static matrixProduct test", () => {
+    let m1 = new Matrix(3, 2);
+    m1.matrix = [
+      [10, -15],
+      [12, 0.5],
+      [5, 16],
+    ];
 
-  let m2 = new Matrix(3, 2);
-  m2.matrix = [
-    [-12, -0.9],
-    [-500, -43],
-    [1, 0.5],
-  ];
+    let m2 = new Matrix(2, 4);
+    m2.matrix = [
+      [12, 9, 0.2, 0.01],
+      [500, 43, 9, -10],
+    ];
 
-  m1.multiplyElementwise(m2);
+    let result = Matrix.matrixProduct(m1, m2);
 
-  expect(m1).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [-120, 13.5],
-      [-6000, -21.5],
-      [5, 8],
-    ],
+    expect(result).toEqual({
+      rows: 3,
+      cols: 4,
+      matrix: [
+        [-7380, -555, -133, 150.1],
+        [394, 129.5, 6.9, -4.88],
+        [8060, 733, 145, -159.95],
+      ],
+    });
   });
 });
 
-test("non-static multiplyScalar test", () => {
-  let m = new Matrix(3, 2);
-  m.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
+describe("Transpose Matrix Tests", () => {
+  test("static transpose test", () => {
+    let m = new Matrix(3, 2);
+    m.matrix = [
+      [1, 5],
+      [2, 0.5],
+      [0.01, 100],
+    ];
 
-  let scalar = -2;
+    let mt = Matrix.transpose(m);
 
-  m.multiplyScalar(scalar);
-
-  expect(m).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [-20, 30],
-      [-24, -1],
-      [-10, -32],
-    ],
+    expect(mt).toEqual({
+      rows: 2,
+      cols: 3,
+      matrix: [
+        [1, 2, 0.01],
+        [5, 0.5, 100],
+      ],
+    });
   });
 });
 
-test("static fromArray test", () => {
-  let array = [12, 10, 5, 4, 3];
-  let m = Matrix.fromArray(array);
+describe("Row Column Removal Tests", () => {
+  test("Row Removal normal", () => {
+    let m = new Matrix(3, 2);
+    m.matrix = [
+      [1, 2],
+      [4, 2],
+      [3, 6],
+    ];
+    let m2 = Matrix.removeRow(m, 2);
+    expect(m2).toEqual({
+      rows: 2,
+      cols: 2,
+      matrix: [
+        [1, 2],
+        [4, 2],
+      ],
+    });
+  });
 
-  expect(m).toEqual({
-    rows: 5,
-    cols: 1,
-    matrix: [[12], [10], [5], [4], [3]],
+  test("Column Removal normal", () => {
+    let m = new Matrix(4, 3);
+    m.matrix = [
+      [2, 1, 3],
+      [2, 5, 6],
+      [10, 8, 6],
+      [27, 45, 3],
+    ];
+    let m2 = Matrix.removeCol(m, 1);
+    expect(m2).toEqual({
+      rows: 4,
+      cols: 2,
+      matrix: [
+        [2, 3],
+        [2, 6],
+        [10, 6],
+        [27, 3],
+      ],
+    });
   });
 });
 
-test("static addElementwise test", () => {
-  let m1 = new Matrix(3, 2);
-  m1.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
+describe("Determinant Tests", () => {
+  test("Base case non-static determinant test", () => {
+    let m = new Matrix(1, 1);
+    m.matrix = [[2]];
+    let det = m.determinant();
 
-  let m2 = new Matrix(3, 2);
-  m2.matrix = [
-    [12, 0.9],
-    [500, 43],
-    [-1, -0.5],
-  ];
-
-  let result = Matrix.addElementwise(m1, m2);
-
-  expect(result).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [22, -14.1],
-      [512, 43.5],
-      [4, 15.5],
-    ],
+    expect(det).toEqual(2);
   });
-});
 
-test("static addScalar test", () => {
-  let m = new Matrix(3, 2);
-  m.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
-
-  let scalar = -9;
-
-  let result = Matrix.addScalar(m, scalar);
-
-  expect(result).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [1, -24],
-      [3, -8.5],
-      [-4, 7],
-    ],
+  test("Normal non-static determinant test", () => {
+    let m = new Matrix(3, 3);
+    m.matrix = [
+      [2, 3, 1],
+      [-4, 2, -1],
+      [10, 1, 5],
+    ];
+    let det = m.determinant();
+    expect(det).toEqual(28);
   });
-});
 
-test("static subtractElementwise test", () => {
-  let m1 = new Matrix(3, 2);
-  m1.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
+  test("Base case static determinant test", () => {
+    let m = new Matrix(1, 1);
+    m.matrix = [[2]];
+    let det = Matrix.determinant(m);
 
-  let m2 = new Matrix(3, 2);
-  m2.matrix = [
-    [-12, -0.9],
-    [-500, -43],
-    [1, 0.5],
-  ];
-
-  let result = Matrix.subtractElementwise(m1, m2);
-
-  expect(result).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [22, -14.1],
-      [512, 43.5],
-      [4, 15.5],
-    ],
+    expect(det).toEqual(2);
   });
-});
 
-test("static subtractScalar test", () => {
-  let m = new Matrix(3, 2);
-  m.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
-
-  let scalar = 9;
-
-  let result = Matrix.subtractScalar(m, scalar);
-
-  expect(result).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [1, -24],
-      [3, -8.5],
-      [-4, 7],
-    ],
-  });
-});
-
-test("static multiplyElementwise test", () => {
-  let m1 = new Matrix(3, 2);
-  m1.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
-
-  let m2 = new Matrix(3, 2);
-  m2.matrix = [
-    [-12, -0.9],
-    [-500, -43],
-    [1, 0.5],
-  ];
-
-  let result = Matrix.multiplyElementwise(m1, m2);
-
-  expect(result).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [-120, 13.5],
-      [-6000, -21.5],
-      [5, 8],
-    ],
-  });
-});
-
-test("static multiplyScalar test", () => {
-  let m = new Matrix(3, 2);
-  m.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
-
-  let scalar = -2;
-
-  let result = Matrix.multiplyScalar(m, scalar);
-
-  expect(result).toEqual({
-    rows: 3,
-    cols: 2,
-    matrix: [
-      [-20, 30],
-      [-24, -1],
-      [-10, -32],
-    ],
-  });
-});
-
-test("static matrixProduct test", () => {
-  let m1 = new Matrix(3, 2);
-  m1.matrix = [
-    [10, -15],
-    [12, 0.5],
-    [5, 16],
-  ];
-
-  let m2 = new Matrix(2, 4);
-  m2.matrix = [
-    [12, 9, 0.2, 0.01],
-    [500, 43, 9, -10],
-  ];
-
-  let result = Matrix.matrixProduct(m1, m2);
-
-  expect(result).toEqual({
-    rows: 3,
-    cols: 4,
-    matrix: [
-      [-7380, -555, -133, 150.1],
-      [394, 129.5, 6.9, -4.88],
-      [8060, 733, 145, -159.95],
-    ],
-  });
-});
-
-test("static transpose test", () => {
-  let m = new Matrix(3, 2);
-  m.matrix = [
-    [1, 5],
-    [2, 0.5],
-    [0.01, 100],
-  ];
-
-  let mt = Matrix.transpose(m);
-
-  expect(mt).toEqual({
-    rows: 2,
-    cols: 3,
-    matrix: [
-      [1, 2, 0.01],
-      [5, 0.5, 100],
-    ],
+  test("Normal static determinant test", () => {
+    let m = new Matrix(3, 3);
+    m.matrix = [
+      [2, 3, 1],
+      [-4, 2, -1],
+      [10, 1, 5],
+    ];
+    let det = Matrix.determinant(m);
+    expect(det).toEqual(28);
   });
 });
